d278e0a6feec7e804d79a2b588e9c1979bef7ef6
toa0904 <56389717+toa0904@users.noreply.github.com>
2019-10-15 17:20:22 +0900

    Revert " データベースの実装"

---
+++ db/migrate/20191015065636_create_groups_users.rb
@@ -0,0 +1,11 @@
+class CreateGroupsUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :groups_users do |t|
+      t.integer :user_id null: false, foreign_key: true
+      t.integer :group_id null: false, foreign_key: true
+
+      t.timestamps
+      
+    end
+  end
+end
